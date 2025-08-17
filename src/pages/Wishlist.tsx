c7aab463af81efc67bda0edc38ea9407a6d93785
Harshith Kakaraparthi <harshithhemanth2006@gmail.com>
2025-08-17 22:09:56 +0530

    Update Wishlist.tsx

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -82,12 +82,12 @@ const Wishlist = () => {
             </Link>
           </div>
         </div>
-      </Layout>
+
     );
   }
 
   return (
-    <Layout>
+  
       <div className="min-h-screen bg-background">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           {/* Header */}
